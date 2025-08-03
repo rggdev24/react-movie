17e9f7570e2662133f2033fc05fe11b853dd03b4
RoyG Gallardo <rggdev24>
2025-08-03 12:12:05 +0800

    Fixing Github Failure

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,66 @@
-import { useEffect, useState } from "react";
-import { Movie, TopMovie } from "./components/Movie";
-import Search from "./components/Search";
-import type { IAppwriteTopMovie, IMovie } from "./interface/Movie";
-import { useDebounce } from "react-use";
-import { getTrendingMovies, updateSearchCount } from "./services/appwrite";
+// import { useEffect, useState } from "react";
+// import { Movie, TopMovie } from "./components/Movie";
+// import Search from "./components/Search";
+// import type { IAppwriteTopMovie, IMovie } from "./interface/Movie";
+// import { useDebounce } from "react-use";
+// import { getTrendingMovies, updateSearchCount } from "./services/appwrite";
 import { TodoMain } from "./components/Todo/TodoMain";
 
-const TMDB_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
-const TMDB_API_READ_ACCESS_TOKEN = import.meta.env
-  .VITE_TMDB_API_READ_ACCESS_TOKEN;
-const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${TMDB_API_READ_ACCESS_TOKEN}`,
-  },
-};
+// const TMDB_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
+// const TMDB_API_READ_ACCESS_TOKEN = import.meta.env
+//   .VITE_TMDB_API_READ_ACCESS_TOKEN;
+// const API_OPTIONS = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization: `Bearer ${TMDB_API_READ_ACCESS_TOKEN}`,
+//   },
+// };
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [movieList, setMovieList] = useState([]);
-  const [debounceSearch, setDebounceSearch] = useState("");
-  const [topMovies, setTopMovies] = useState<IAppwriteTopMovie[]>([]);
+  // const [search, setSearch] = useState("");
+  // const [errorMessage, setErrorMessage] = useState("");
+  // const [movieList, setMovieList] = useState([]);
+  // const [debounceSearch, setDebounceSearch] = useState("");
+  // const [topMovies, setTopMovies] = useState<IAppwriteTopMovie[]>([]);
 
-  const loadTrendingMoviews = async () => {
-    try {
-      const movies = await getTrendingMovies();
-      if (movies) {
-        setTopMovies(movies);
-      }
-    } catch (error) {}
-  };
+  // const loadTrendingMoviews = async () => {
+  //   try {
+  //     const movies = await getTrendingMovies();
+  //     if (movies) {
+  //       setTopMovies(movies);
+  //     }
+  //   } catch (error) {}
+  // };
 
-  const fetchMovie = async (query = "") => {
-    try {
-      const endpoint = query
-        ? `${TMDB_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-        : `${TMDB_BASE_URL}/discover/movie?sort_by=popularity.desc`;
-      const response = await fetch(endpoint, API_OPTIONS);
-      const data = await response.json();
+  // const fetchMovie = async (query = "") => {
+  //   try {
+  //     const endpoint = query
+  //       ? `${TMDB_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+  //       : `${TMDB_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  //     const response = await fetch(endpoint, API_OPTIONS);
+  //     const data = await response.json();
 
-      setMovieList(data.results || []);
+  //     setMovieList(data.results || []);
 
-      if (query && data.results.length > 0) {
-        await updateSearchCount(query, data.results[0]);
-      }
-    } catch (error) {
-      console.log(`Error fetching movies `, error);
-      setErrorMessage(`Error fetching movies. Please try again later`);
-    }
-  };
+  //     if (query && data.results.length > 0) {
+  //       await updateSearchCount(query, data.results[0]);
+  //     }
+  //   } catch (error) {
+  //     console.log(`Error fetching movies `, error);
+  //     setErrorMessage(`Error fetching movies. Please try again later`);
+  //   }
+  // };
 
-  useEffect(() => {
-    loadTrendingMoviews();
-  }, []);
+  // useEffect(() => {
+  //   loadTrendingMoviews();
+  // }, []);
 
-  useEffect(() => {
-    fetchMovie(debounceSearch);
-  }, [debounceSearch]);
+  // useEffect(() => {
+  //   fetchMovie(debounceSearch);
+  // }, [debounceSearch]);
 
-  useDebounce(() => setDebounceSearch(search), 1000, [search]);
+  // useDebounce(() => setDebounceSearch(search), 1000, [search]);
 
   return (
     <main>
